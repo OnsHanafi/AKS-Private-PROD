5a4a55368ac0032be57006dbd5b6371a6c3b54d1
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2025-08-17 03:39:21 +0200

    AZ AUTH

--- 01-provider.tf
+++ 01-provider.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
     features {}
     skip_provider_registration = true
-    subscription_id = "00000000-0000-0000-0000-000000000000"
-    tenant_id       = "11111111-1111-1111-1111-111111111111"
+    subscription_id = var.subscription_id
+    tenant_id       = var.tenant_id
 }
 
 terraform {
