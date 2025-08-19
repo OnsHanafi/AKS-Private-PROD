2d580c1235cbf5397528010cebb826a62154125d
OnsHanafi <ons.hanafi2@gmail.com>
2025-08-19 00:27:19 +0000

    AKS & JUMPBOX DEPLOYED

--- 00-vars.tf
+++ 00-vars.tf
@@ -8,7 +8,8 @@ variable "resource_group_name" { default = "rg-leadbook" }
 # variable "aks_version" { default = "1.29.8" }
 variable "aks_name" { default = "leadbook-cluster"}
 variable "aks_vm_size" {default = "Standard_B2ms"}
-variable "jumpbox_size" { default = "Standard_B2s" }
+variable "aks_spot_size" {default = "Standard_D2s_v3"}
+variable "jumpbox_size" { default = "Standard_F2s_v2" }
 variable "acr_name" { default = "leadbookacr" }
 
 
--- 06-aks.tf
+++ 06-aks.tf
@@ -59,39 +59,40 @@ resource "azurerm_kubernetes_cluster" "aks" {
 # -------------------------
 # Node POOL - For Autoscaling
 # -------------------------
-resource "azurerm_kubernetes_cluster_node_pool" "spot" {
-  name                  = "spot"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  vm_size               = var.aks_vm_size
-  vnet_subnet_id        = azurerm_subnet.aks_subnet.id
+#resource "azurerm_kubernetes_cluster_node_pool" "spot" {
+#  name                  = "spot"
+#  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+#  vm_size               = var.aks_vm_size
+#  vm_size               = var.aks_spot_size
+#  vnet_subnet_id        = azurerm_subnet.aks_subnet.id
 #  orchestrator_version  = var.aks_version
-  priority              = "Spot"
-  spot_max_price        = -1
-  eviction_policy       = "Delete"
+#  priority              = "Spot"
+#  spot_max_price        = -1
+#  eviction_policy       = "Delete"
 
-  auto_scaling_enabled = true
-  node_count          = 1
-  min_count           = 1
-  max_count           = 10
+#  auto_scaling_enabled = true
+#  node_count          = 1
+#  min_count           = 1
+#  max_count           = 10
 
-  node_labels = {
-    role                                    = "spot"
-    "kubernetes.azure.com/scalesetpriority" = "spot"
-  }
+#  node_labels = {
+#    role                                    = "spot"
+#    "kubernetes.azure.com/scalesetpriority" = "spot"
+#  }
   
 
-  node_taints = [
-    "spot:NoSchedule",
-    "kubernetes.azure.com/scalesetpriority=spot:NoSchedule"
-  ]
+#  node_taints = [
+#    "spot:NoSchedule",
+#   "kubernetes.azure.com/scalesetpriority=spot:NoSchedule"
+#  ]
 
 
   
 
-  lifecycle {
-    ignore_changes = [node_count]
-  }
-}
+#  lifecycle {
+#    ignore_changes = [node_count]
+#  }
+#}
 
 
 
